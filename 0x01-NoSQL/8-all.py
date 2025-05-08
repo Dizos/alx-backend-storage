5adad23a86d61d117df9999e35e0ec87ed29e6c8
Dizos <tandomjekula@gmail.com>
2025-05-08 10:56:58 +0200

    update

--- 0x01-NoSQL/8-all.py
+++ 0x01-NoSQL/8-all.py
@@ -1,20 +1,14 @@
 #!/usr/bin/env python3
-"""
-Module for listing all documents in a MongoDB collection
-"""
-from pymongo.collection import Collection
-from typing import List, Dict, Any
+"""Module to list all documents in a MongoDB collection"""
 
-
-def list_all(mongo_collection: Collection) -> List[Dict[str, Any]]:
+def list_all(mongo_collection):
     """
-    Lists all documents in the specified MongoDB collection.
+    Lists all documents in a MongoDB collection.
 
     Args:
-        mongo_collection (Collection): The PyMongo collection object.
+        mongo_collection: PyMongo collection object
 
     Returns:
-        List[Dict[str, Any]]: A list of all documents in the collection,
-                              or an empty list if the collection is empty.
+        list: List of documents in the collection, or empty list if none
     """
     return list(mongo_collection.find())
